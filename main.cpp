d7ccda2afea349d7f135ab93a5dc36a5bad65323
OBess <dima.suschenko03@gmail.com>
2021-08-09 13:42:19 +0300

    Added example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 // C++
 #include <iostream>
+#include <memory>
 
-int main(int argc, char const *argv[])
+// Custom
+#include "include/factory/console_creator.hpp"
+#include "include/factory/file_creator.hpp"
+
+int main()
 {
-    std::cout << "M" << std::endl;
-    return EXIT_SUCCESS;
+   
+   // Test 1 - console logger
+   auto console_cr = std::make_unique<console_creator<console_logger>>();
+   auto console = console_cr->getLogger();
+   console->Log("Start work");
+   console->Log("Did something");
+   console->Log("End work");
+   console->Log("Message ", "with ", "variability size of", "values", " and ", 'i', 'n', " this ", "text are ", 10, " values.");
+   console->Log(12, ' ', 3.5);
+
+   // Test 2 - file logger
+   auto file_cr = std::make_unique<file_creator<file_logger>>("log.txt");
+   auto file = file_cr->getLogger();
+   file->Log("Start work");
+   file->Log("Did something");
+   file->Log("End work");
+   file->Log("Message ", "with ", "variability size of", "values", " and ", 'i', 'n', " this ", "text are ", 10, " values.");
+   file->Log(12, ' ', 3.5);
+
+   // Clear data
+   delete console;
+   delete file;
+
+   return EXIT_SUCCESS;
 }
